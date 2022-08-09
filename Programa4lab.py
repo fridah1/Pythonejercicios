70d41627296eb46384576595d78573ea4a35f9f2
fridah1 <109167102+fridah1@users.noreply.github.com>
2022-08-09 13:38:01 -0600

    Create Programa4lab.py

---
+++ Programa4lab.py
@@ -0,0 +1,6 @@
+#aquí pedimos que ingrese dos numeros
+numero= int(input("Ingrese un numero: "))
+numero2= int(input("Ingrese un numero: "))
+#Aquí es cuando se divide para que nos de la raíz
+respuesta = numero**(1/numero2)
+print(respuesta)
