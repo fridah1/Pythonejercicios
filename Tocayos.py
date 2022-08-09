ed171c227233763bab6070adc51f2dc2bc2c9273
fridah1 <109167102+fridah1@users.noreply.github.com>
2022-08-09 12:52:12 -0600

    Create Tocayos.py

---
+++ Tocayos.py
@@ -0,0 +1,10 @@
+#Ingresas dos nombres y te indicia si son iguales o no.
+n3=str(input("ingrese nombre 1"))
+
+n4=str(input("ingrese nombre 2"))
+
+if n3==n4:
+  print ("si son tocayos")
+
+else: 
+    print ("no son tocayos")
