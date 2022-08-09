31952ce54c3213bd678b0ca20073f0316bb939a4
fridah1 <109167102+fridah1@users.noreply.github.com>
2022-08-09 12:48:47 -0600

    Create Condicional.py

---
+++ Condicional.py
@@ -0,0 +1,9 @@
+#Ingresas un numero y te indica si es par o impar. 
+n1=int(input("ingrese un numero"))
+res=n1%2
+print(res)
+if res== 0: 
+  print("el numero que ingreso es par")
+  else
+print("el numero que ingreso es impar")
+print(res)
