085e4aff598e69ddc23ae891fabf044e1816350b
fridah1 <109167102+fridah1@users.noreply.github.com>
2022-08-09 13:43:34 -0600

    Create Examenlab.py

---
+++ Examenlab.py
@@ -0,0 +1,41 @@
+#Es un menú, y te pregunta que ingredientes quieres.
+lista=[]
+print("Menú HAmburCat")
+print("Indique Si o No, si quieres los siguientes ingredientes para su hamburguesa: ")
+
+#while clave != a3:
+ #a=int(input(""))
+print("Si o no")
+a=str(input("¿Desea torta de carne?"))
+aaaa= "si"
+aa="no"
+if a == "si":
+  print("Usted tiene carne en su hamburguesa")
+  lista.append("Si va a tener carne")
+else: 
+  print("No va a tener carne")
+  
+c=int(input("¿Desea torta de carne extra?"))
+print("Si o no")
+d=int(input("¿Desea tocino?"))
+print("Si o no")
+e=int(input("¿Desea lechuga?"))
+print("Si o no")
+f=int(input("¿Desea tomate?"))
+print("Si o no")
+g=int(input("¿Desea cebolla?"))
+print("Si o no")
+h=int(input("¿Desea mayonesa?"))
+print("Si o no")
+i=int(input("¿Desea mostaza?"))
+print("Si o no")
+j=int(input("¿Desea ketchup?"))
+print("Si o no")
+k=int(input("¿Desea algun acompañamiento?"))
+print("Si o no")
+l=int(input("¿Desea papas?"))
+print("Si o no")
+m=int(input("¿Desea aros de cebolla?"))
+print("Si o no")
+n=int(input(""))
+ 
