b945607421f795f597cf82e1a8995e5a59958a90
fridah1 <109167102+fridah1@users.noreply.github.com>
2022-08-09 13:01:53 -0600

    Create Programa4.py

---
+++ Programa4.py
@@ -0,0 +1,17 @@
+#Ingresas un numero del 1 al 7 que representan los dias de la semana, y dependiendo el que psuiste te ingresa una frase diferente. 
+n1=int(input("Ingrese un numero del 1 al 7:"))
+
+if n1==1: 
+ print("Que tengas un lindo inicio de semana")
+
+ if n1==5: 
+  print ("Es viernes y mi cuerpo lo sabe")
+
+ if n1==6: 
+  print("Sábado debería escribirse con F de felicidad")
+
+ if n1==7: 
+  print("¡Feliz domingo! Aprovecha para descansar, pues la semana pronto vuelve a comenzar")
+
+else:
+  print("Que tengas un grandioso dia :)")
