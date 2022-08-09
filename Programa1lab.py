35f365551b01eddc4366526acd02f3c1bd3b0973
fridah1 <109167102+fridah1@users.noreply.github.com>
2022-08-09 13:36:24 -0600

    Create Programa1lab.py

---
+++ Programa1lab.py
@@ -0,0 +1,19 @@
+#Aquí se pide que se ingresen los 10 número enteros, luego suma el primer y el décimo número, y luego se suma del segundo al noveno número de la lista.
+
+a1=int(input("ingrese primer numero entero: "))
+a2=int(input("ingrese segundo entero: "))
+a3=int(input("ingrese tercer numero entero: "))
+a4=int(input("ingrese cuarto numero entero: "))
+a5=int(input("ingrese quinto numero entero: "))
+a6=int(input("ingrese sexto numero entero: "))
+a7=int(input("ingrese septimo numero entero: "))
+a8=int(input("ingrese octavo numero entero: "))
+a9=int(input("ingrese noveno numero entero: "))
+a10=int(input("ingrese decimo numero entero: "))
+#Aquí se van a sumar el primero y el decimo numero
+res=a1+a10
+print(res)
+#Aquí se suman del segundo al noveno número de la lista.
+res=a2+a3+a4+a5+a6+a7+a8+a9
+print(res)
+
