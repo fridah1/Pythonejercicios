6e07d3e994e87071b83bb502adce031c12bc8f51
fridah1 <109167102+fridah1@users.noreply.github.com>
2022-08-09 12:56:58 -0600

    Create Programa2.py

---
+++ Programa2.py
@@ -0,0 +1,11 @@
+#Ingresas dos numeros y te indica cual de los dos es menor. 
+n1=int(input("Ingrese numero 1:"))
+n2=int(input("Ingrese numero 2:"))
+
+if n1<n2:
+  print("El primer numero es menor")
+if n2<n1:
+  print("El segundo numero es menor")
+if n1==n2:
+    print ("Lo siento, ambos numeros son iguales, trata con dos diferentes :)")
+  
